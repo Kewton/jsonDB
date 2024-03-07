a97d4436141bc0879f15bfac95e400ab8a3186dc
kewton <newtons.boiled.clock@gmail>
2024-03-07 23:33:31 +0900

    docを追加

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='localjsondb',  # パッケージ名
-    version='0.1.2',  # バージョン
+    version='0.1.3',  # バージョン
     author='kewton',  # 著者名
     author_email='newtons.boiled.clock@gmail.com',  # 著者のメールアドレス
     description='A short description of the package',  # パッケージの短い説明
     #long_description=open('README.md').read(),  # 長い説明（通常はREADME）
     #long_description_content_type='text/markdown',  # 長い説明のコンテントタイプ
     url='https://github.com/Kewton/jsonDB',  # プロジェクトのURL
-    packages=find_packages(exclude=('tutorial')),  # パッケージのインクルード（テストやドキュメントを除外）
+    packages=find_packages(exclude=('tutorial',"docs", "tests")),  # パッケージのインクルード（テストやドキュメントを除外）
     install_requires=[  # 依存関係のリスト
         'pysondb',  # 例: NumPy 1.18.1以上
         'pydantic',  # 例: Requests 2.22.0以上
--- tests/test_my_module.py
+++ tests/test_my_module.py
@@ -1,2 +1,70 @@
+import sys
+import os
+
+# このスクリプトの親ディレクトリへのパスを取得
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+# sys.pathに親ディレクトリを追加
+if parent_dir not in sys.path:
+    sys.path.insert(0, parent_dir)
+
+from localjsondb.jsonDB import ValidatedSchemaFactory, BaseJsonDbORM, DoFactory
+
+
+class _MyData_Prop:
+    name: str
+    email: str
+    data: dict
+    uso: str = "aaaaaa"
+
+class _MyData_Schema(_MyData_Prop, ValidatedSchemaFactory):
+    pass
+
+class MyData_Do(_MyData_Prop, DoFactory):
+    pass
+
+class MyJson_ORM(BaseJsonDbORM):
+    dbpath = "./tests/mydb/test"
+    schema = _MyData_Schema 
+
+    def __init__(self, _dbname):
+        self.dbname = _dbname
+        super().__init__()
+    
+class MyJson_ORM2(BaseJsonDbORM):
+    dbpath = "./tests/mydb/test2"
+    schema = _MyData_Schema 
+    dbname = "single"
+
+# myJson_ORM_1 = MyJson_ORM("db_test1")
+#myJson_ORM_2 = MyJson_ORM2()
+#myJson_ORM_3 = MyJson_ORM2()
+
 def test_add():
-    assert True
+    myJson_ORM_1 = MyJson_ORM("db_test1")
+    if os.path.exists(os.path.join("./tests/mydb/test","db_test1.json")):
+        assert True
+    else:
+        assert False
+    
+    data = MyData_Do()
+    data.name = "aaaaaaaaaa"
+    data.email = "test@ma"
+    data.data = {
+        "test":"aaaaaaaaaaaaaaaaaaaaaaa_11111111111111",
+        "hogehoge":"uoooooooooooooooooo",
+        "aaa": [
+            {"a":1},
+            {"a":2},
+            {"a":3},
+            {"a":4},
+            {"a":5}
+        ]
+    }
+
+    myJson_ORM_1.upsert(data)
+    for a in myJson_ORM_1.jsondb.getByQuery(data.to_query_dict()):
+        _data = MyData_Do().from_json_dict(a)
+        if data.name == _data.name:
+            assert True
+        else:
+            assert False
