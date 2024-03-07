85abb5e2294446a495b432815b644ad2421e7bea
kewton <newtons.boiled.clock@gmail>
2024-03-07 22:29:04 +0900

    setupの修正

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     author='kewton',  # 著者名
     author_email='newtons.boiled.clock@gmail.com',  # 著者のメールアドレス
     description='A short description of the package',  # パッケージの短い説明
-    long_description=open('README.md').read(),  # 長い説明（通常はREADME）
-    long_description_content_type='text/markdown',  # 長い説明のコンテントタイプ
+    #long_description=open('README.md').read(),  # 長い説明（通常はREADME）
+    #long_description_content_type='text/markdown',  # 長い説明のコンテントタイプ
     url='https://github.com/Kewton/jsonDB',  # プロジェクトのURL
     packages=find_packages(exclude=('tutorial')),  # パッケージのインクルード（テストやドキュメントを除外）
     install_requires=[  # 依存関係のリスト
